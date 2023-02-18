b1823c2acff58932ea741256942a07290e9756e3
Anne Caroline Rocha <carolsprak@gmail.com>
2023-02-18 03:00:07 -0300

    Criacao do repositorio git para o projeto

---
+++ src/main/java/com/ultimaschool/java/Main.java
@@ -0,0 +1,17 @@
+package com.ultimaschool.java;
+
+import com.ultimaschool.java.clientes.Cliente;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente clienteF = new Cliente("123.456.789-00", "joana@mail.com", "Joana Maria Rocha",
+                'F');
+        System.out.println(clienteF.toString());
+
+        Cliente clienteM = new Cliente("999.888.777-55", "rogerio@mail.com", "Rogério Azevedo",
+                'M');
+
+        System.out.println(clienteM.toString());
+    }
+}
