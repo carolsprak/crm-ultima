b76a7dac9cff6cc4df557bba739bbddfa9c0caa8
Anne Caroline Rocha <carolsprak@gmail.com>
2023-02-26 04:06:52 -0300

    Criacao de classe Cliente usando o padrao de projeto Builder

--- src/main/java/com/ultimaschool/java/Main.java
+++ src/main/java/com/ultimaschool/java/Main.java
@@ -1,6 +1,7 @@
 package com.ultimaschool.java;
 
 import com.ultimaschool.java.clientes.Cliente;
+import com.ultimaschool.java.clientes.ClienteBuilder;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +16,27 @@ public class Main {
                 "(83) 8888-8888");
 
         System.out.println(clienteM.toString());
+
+        ClienteBuilder clienteFCompleto = new ClienteBuilder().comIdentificacao("Joana", "Maria",
+                "Rocha","123.456.789-00",
+                "10/01/1993", 'F').comContatos("joana@mail.com", "Av Brasil",
+                "(83) 9999-9999");
+        System.out.println(clienteFCompleto.toString());
+
+        ClienteBuilder clienteMContatos = new ClienteBuilder();
+
+        ClienteBuilder clienteMIdentificacao = new ClienteBuilder().comIdentificacao("Rogério",
+                "", "Azevedo",
+                "999.888.777-55", "10/01/1983", 'M');
+
+        System.out.println(clienteMIdentificacao.toStringIndentificacao());
+
+        clienteMContatos = new ClienteBuilder().comContatos("rogerio@mail.com", "Av Brasil",
+                "(83) 8888-8888");
+
+        System.out.println(clienteMContatos.toStringContatos());
+
+
+
     }
 }
